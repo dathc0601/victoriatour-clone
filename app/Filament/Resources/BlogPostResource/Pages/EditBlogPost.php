93341a625e3cecd9fff5bc57f779b798c2fffef2
dcstylexf <dcstylexf@gmail.com>
2025-12-23 23:51:57 +0700

    Update multiple translation services

--- app/Filament/Resources/BlogPostResource/Pages/EditBlogPost.php
+++ app/Filament/Resources/BlogPostResource/Pages/EditBlogPost.php
@@ -9,6 +9,7 @@ use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
+use Illuminate\Support\Facades\Log;
 
 class EditBlogPost extends EditRecord
 {
@@ -28,21 +29,39 @@ class EditBlogPost extends EditRecord
                 ->modalHeading('Trigger Translation')
                 ->modalDescription('This will queue this content for immediate translation to all active languages.')
                 ->action(function () {
-                    $record = $this->record;
-                    $sourceLocale = $record->source_locale ?? 'en';
-                    $targetLocales = Language::active()
-                        ->where('code', '!=', $sourceLocale)
-                        ->pluck('code');
+                    try {
+                        Log::info('Translate Now button clicked', ['record_id' => $this->record->id]);
 
-                    foreach ($targetLocales as $locale) {
-                        TranslateModel::dispatch($record, $locale, force: true);
+                        $record = $this->record;
+                        $sourceLocale = $record->source_locale ?? 'en';
+                        $targetLocales = Language::active()
+                            ->where('code', '!=', $sourceLocale)
+                            ->pluck('code');
+
+                        Log::info('Target locales', ['locales' => $targetLocales->toArray()]);
+
+                        foreach ($targetLocales as $locale) {
+                            TranslateModel::dispatch($record, $locale, force: true);
+                            Log::info('Dispatched translation job', ['locale' => $locale]);
+                        }
+
+                        Notification::make()
+                            ->title('Translation queued')
+                            ->body('Content has been queued for translation.')
+                            ->success()
+                            ->send();
+                    } catch (\Exception $e) {
+                        Log::error('Translate Now button error', [
+                            'error' => $e->getMessage(),
+                            'trace' => $e->getTraceAsString(),
+                        ]);
+
+                        Notification::make()
+                            ->title('Translation failed')
+                            ->body($e->getMessage())
+                            ->danger()
+                            ->send();
                     }
-
-                    Notification::make()
-                        ->title('Translation queued')
-                        ->body('Content has been queued for translation.')
-                        ->success()
-                        ->send();
                 }),
             Actions\DeleteAction::make(),
         ];
